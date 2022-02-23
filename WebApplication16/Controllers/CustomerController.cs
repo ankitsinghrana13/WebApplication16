0929d3c49a56bc6f6a538c3a117e02af9a0eab9a
ankitsinghrana13 <ak9897432538@gmail.com>
2022-02-24 06:08:13 +0800

    Add project files.

---
+++ WebApplication16/Controllers/CustomerController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication16.Models;
+
+namespace WebApplication16.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private ApplicationDbContext dbContext;
+        public CustomerController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            List<Location> locations = dbContext.Locations.ToList();
+            return View(locations);
+        }
+        public IActionResult CustomerList(int id)
+        {
+            List<Customer> customers = dbContext.Customers.Where(e => e.Location.Id == id).ToList();
+            return View(customers);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Employee emp)
+        {
+           dbContext.Employees.Add(emp);
+            dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        
+
+        }
+    }
+
---
+++ WebApplication16/Controllers/EmployeeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication16.Models;
+namespace WebApplication16.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private ApplicationDbContext dbContext;
+            public EmployeeController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            List<Employee> Employees = dbContext.Employees.ToList();
+            return View(Employees);
+        }
+    }
+}
